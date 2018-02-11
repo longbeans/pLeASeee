3a30350a299d37b3a822766bd6c95d178f65c25f
longbeans <36259551+longbeans@users.noreply.github.com>
2018-02-11 17:52:27 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -7,8 +7,18 @@
 	<td><a href="showprod.php"><button type="submit" value="Submit" class="button"><span>View Products</span></button></td>
 	<td><a href="logout.php"><button type="submit" value="Submit" class="button"><span>Log out</span></button></td>
 </tr>
-	
-
+</table>
+<?php
+	session_start();
+	if(time()- $_SESSION["time"] > 600)
+	{
+		header("Location:logout.php");
+	}
+	else
+	{
+		$_SESSION["time"] = time();
+	}
+?>
 	
 </body>
 </html>
